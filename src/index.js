37683afd14487fd42937e6d4224d53521312cf6f
Tobias Santos <93166140+tobiasPaes@users.noreply.github.com>
2023-06-23 23:03:44 -0300

    final index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,28 @@ const dotenv = require('dotenv').config()
 
 const PORT = process.env.PORT || 3333
 
-const pool = new Pool({
+// const pool = new Pool({
   
-    database: process.env.POSTGRES_DATABASE,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT
-})
+//     database: process.env.POSTGRES_DATABASE,
+//     user: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD,
+//     port: process.env.POSTGRES_PORT
+// })
+
+const postgres = require('postgres');
+require('dotenv').config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+const sql = postgres(URL, { ssl: 'require' });
+
+async function getPgVersion() {
+  const result = await sql`select version()`;
+//   console.log(result);
+}
+
+getPgVersion();
 
 const app = express()
 
@@ -19,62 +34,71 @@ app.use(express.json())
 
 // app.use(cors())
 
-app.get('/', (req, res) => {console.log('ola mundo')})
-
 app.get('/carros', async (req, res) => {
-    try{
-        const { rows } = await pool.query('SELECT * from carro')
-        return res.status(200).send(rows)
-    }catch(err){
-        return res.status(400).send(err)
-    }
+    const carro = await sql`select * from carro`
+    return res.status(200).send(carro)
+  // try{
+    //     const { rows } = await pool.query('SELECT * from carro')
+    //     return res.status(200).send(rows)
+    // }catch(err){
+    //     return res.status(400).send(err)
+    // }
 })
 
 app.get('/carros/:carro_id', async (req, res) => {
     const {carro_id} = req.params
-    try{
-        const carro = await pool.query('SELECT * from carro where id = $1', [carro_id])
-        return res.status(200).send(carro.rows)
-    }catch(err){
-        console.log(err)
-        return res.status(400).send(err)
-    }
+    const carro = await sql`select * from carro where id = ${carro_id}`
+    return res.status(200).send(carro)
+    // try{
+    //     const carro = await pool.query('SELECT * from carro where id = $1', [carro_id])
+    //     return res.status(200).send(carro.rows)
+    // }catch(err){
+    //     console.log(err)
+    //     return res.status(400).send(err)
+    // }
 })
 
 app.post('/carros', async (req, res) => {
     const {modelo, marca} = req.body
-    let car = ''
-    try {
-        car = await pool.query('SELECT * from carro where marca = $1 and modelo = $2', [marca, modelo])
-        if (!car.rows[0]) {
-            car = await pool.query('INSERT INTO carro (marca, modelo) VALUES ($1, $2) returning *', [marca, modelo])
-        }
-        return res.status(201).send(car.rows)
-    } catch (err) {
-        console.log(err)
-        return res.status(400).send(err)
-    }
+    await sql`insert into carro (marca, modelo) values (${marca}, ${modelo})`
+    return res.status(201).send('criado')
+    // let car = ''    
+    // try {
+    //     car = await pool.query('SELECT * from carro where marca = $1 and modelo = $2', [marca, modelo])
+    //     if (!car.rows[0]) {
+    //         car = await pool.query('INSERT INTO carro (marca, modelo) VALUES ($1, $2) returning *', [marca, modelo])
+    //     }
+    //     return res.status(201).send(car.rows)
+    // } catch (err) {
+    //     console.log(err)
+    //     return res.status(400).send(err)
+    // }
 })
 
 app.patch('/carros/:carro_id', async (req, res) => {
     const {carro_id} = req.params
     const carro = req.body
-    try {
-        const att = await pool.query('update carro set marca = $1, modelo = $2 where id = $3 returning *', [carro.marca, carro.modelo, carro_id])
-        return res.status(200).send(att.rows)
-    } catch (err) {
-        return res.status(400).send(err)
-    }
+    await sql`update carro set marca = ${carro.marca}, modelo = ${carro.modelo} where id = ${carro_id}`
+    const car = await sql`select * from carro where id = ${carro_id}`
+    return res.status(200).send(car)
+    // try {
+    //     const att = await pool.query('update carro set marca = $1, modelo = $2 where id = $3 returning *', [carro.marca, carro.modelo, carro_id])
+    //     return res.status(200).send(att.rows)
+    // } catch (err) {
+    //     return res.status(400).send(err)
+    // }
 })
 
 app.delete('/carros/:carro_id', async (req, res) => {
     const {carro_id} = req.params
-    try {
-        const deletar = await pool.query('delete from carro where id = $1', [carro_id])
-        return res.status(200).send(deletar)
-    } catch (err) {
-        return res.status(400).send(err)
-    }
+    await sql`delete from carro where id = ${carro_id}`
+    return res.status(200).send('deletado')
+    // try {
+    //     const deletar = await pool.query('delete from carro where id = $1', [carro_id])
+    //     return res.status(200).send(deletar)
+    // } catch (err) {
+    //     return res.status(400).send(err)
+    // }
 })
 
 app.listen(PORT, () => console.log(`Server runnig on port ${PORT}`))
